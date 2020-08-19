0ffdde92f027872cf7739189a4ab2c06bfb583f9
joshSteadman <josh.steadman15@gmail.com>
2020-08-19 14:43:41 +0100

    Player2 hp test made

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 require 'rspec'
 
 class Battle < Sinatra::Base
-    get '/' do
+   enable :sessions
+    
+   get '/' do
         erb(:index)
     end
 
     post '/players' do
-      @Player_1 = params[:Player_1]
-      erb(:play)
+      session[:Player_1] = params[:Player_1]
+      session[:Player_2] = params[:Player_2]
+      redirect '/play'
+    end
+
+    get '/play' do
+        @Player_1 = session[:Player_1]
+        @Player_2 = session[:Player_2]
+        erb(:play)
     end
     run! if app_file == $0
 end
--- spec/features/feature_test_spec.rb
+++
@@ -1,9 +0,0 @@
-
-feature 'form test' do
-  scenario 'see players names on screen' do
-    visit('/')
-    fill_in(:Player_1, with: 'John')
-    click_button('Submit')
-    expect(page).to have_content('John')
-  end
-end
